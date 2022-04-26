2791f3cddd032b9b67a1581be5bfcb1d2aff147e
Aydin Tekin <aydin.tekin@ibsolution.de>
2022-04-26 02:49:53 +0200

    fixed indicator on cryptos for low amount of new coins

--- src/components/table/deltacryptoindicator.js
+++ src/components/table/deltacryptoindicator.js
@@ -4,7 +4,7 @@ import DeltaIndicator from "./deltaindicator";
 
 class DeltaCryptoIndicator extends DeltaIndicator {
   render() {
-    return this.getRoundedNumber(this.props.value) != 0 ? (
+    return this.props.value != 0 ? (
       <div className={"relative w-auto pl-1 flex flex-row items-center text-xs " + (this.props.value >= 0 ? "text-green-800 bg-green-200" : "text-red-800 bg-red-200") + " rounded-md "}>
         <div className="text-base text-xs font-bold">{(this.props.value >= 0 ? "+" : "") + toFixed(this.props.value) + "(" + toEuro(this.props.value * this.props.price) + ")"}</div>
       </div>
