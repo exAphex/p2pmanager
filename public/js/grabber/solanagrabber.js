33bf251acc12b6bb1bdb23057f4eb5bd9faafce4
Aydin Tekin <aydin.tekin@ibsolution.de>
2022-07-15 00:38:44 +0200

    fixed solana grabber

--- public/js/grabber/solanagrabber.js
+++ public/js/grabber/solanagrabber.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const api = 'https://api.solscan.io/account?address=';
-const apiStake = 'https://api.solscan.io/account/stake?address=';
+const apiStake = 'https://public-api.solanabeach.io/v1/account/';
 const priceAPI = 'https://api.coingecko.com/api/v3/simple/price?vs_currencies=eur&ids=solana';
 
 const getSolana = async (address) => {
@@ -9,15 +9,15 @@ const getSolana = async (address) => {
   let total = json.data.lamports;
   let stake = 0;
 
-  const responseStake = await fetch(apiStake + address, {method: 'GET', headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'User-Agent': 'PostmanRuntime/7.26.8'}});
+  const responseStake = await fetch(apiStake + address + "/stakes?limit=10&offset=0", {method: 'GET', headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'User-Agent': 'PostmanRuntime/7.26.8'}});
   const jsonStake = await responseStake.json();
 
   const stakeAccounts = jsonStake.data;
   for (const acc in stakeAccounts) {
     if (stakeAccounts.hasOwnProperty(acc)) {
       const obj = stakeAccounts[acc];
-      total += obj.amount * 1;
-      stake += obj.amount * 1;
+      total += obj.lamports * 1;
+      stake += obj.lamports * 1;
     }
   }
 
