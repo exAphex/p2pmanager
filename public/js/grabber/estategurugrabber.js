e515224dd950287d4f0e3c4499d99be3e8addcb3
Aydin Tekin <aydin.tekin@ibsolution.de>
2022-10-24 12:49:28 +0200

    fixed estateguru grabber

--- public/js/grabber/estategurugrabber.js
+++ public/js/grabber/estategurugrabber.js
@@ -1,19 +1,36 @@
 const fetch = require('node-fetch');
-const FormData = require('form-data');
+const fetchCookie = require('fetch-cookie');
 
-const api = 'https://estateguru.co/portal/portfolio/account?lang=en';
-const authAPI = 'https://estateguru.co/portal/login/authenticate';
+const fetchCo = fetchCookie(fetch);
+const headers = {
+  'Content-Type': 'application/json',
+};
+const api = 'https://app.estateguru.co/portfolio/overview';
+const authAPI = 'https://account.estateguru.co/api/login';
+const defaultAccountAPI = 'https://account.estateguru.co/api/user/info?username=';
 
 const getEstateGuru = async (username, password) => {
-  const retAuthData = await getAuthToken(username, password);
-  const response = await fetch(api, {method: 'GET', headers: {cookie: retAuthData}});
+  const data = {username: username, rememberMe:false, password: password};
+  await fetchCo(authAPI, {method: 'POST', headers: headers, body: JSON.stringify(data)})
+  const defaultAccount = await getDefaultAccountId(username);
+  await fetchCo('https://account.estateguru.co/api/account/' + defaultAccount + '/switch', {method: 'POST'});
+  await fetchCo('https://account.estateguru.co/api/account/' + defaultAccount, {method: 'GET'});
+  const response = await fetchCo(api, {method: 'GET'});
   const respText = await response.text();
   const retObj = parseTotal(respText);
   return retObj;
 };
 
+const getDefaultAccountId = async (username) => {
+  const responseS = await fetchCo(defaultAccountAPI + username, {method: 'GET'});
+  const respJSON = await responseS.json();
+  return respJSON.defaultAccountId;
+};
+
 const parseTotal = (data) => {
   let parseObj = parseNextValue(data, '<h4 class="card-title">', '</h4>');
+  parseObj = parseNextValue(parseObj.remain, '<h4 class="card-title">', '</h4>');
+  parseObj = parseNextValue(parseObj.remain, '<h4 class="card-title">', '</h4>');
   const uninvested = parseFloat(parseObj.value.substr(parseObj.value.indexOf('€') + 1).replaceAll(',', ''));
   parseObj = parseNextValue(parseObj.remain, '<span class="item-value">', '</span>');
   const total = parseFloat(parseObj.value.substr(parseObj.value.indexOf('€') + 1).replaceAll(',', ''));
@@ -42,20 +59,6 @@ const parseNextValue = (data, findStr, finishStr) => {
   const newData = data.substr(itemValue + findStr.length);
   const finishPos = newData.indexOf(finishStr);
   return {value: newData.substr(0, finishPos).trim(), remain: data.substr(itemValue + findStr.length + finishPos)};
-};
-
-const getAuthToken = async (username, password) => {
-  const form = new FormData();
-  form.append('username', username);
-  form.append('password', password);
-
-  const response = await fetch(authAPI, {method: 'POST', body: form});
-  let cookieHeaders = response.headers.get('set-cookie');
-  const posJSESSIONID = cookieHeaders.indexOf('JSESSIONID=');
-  cookieHeaders = cookieHeaders.substr(posJSESSIONID);
-  const posSemicolon = cookieHeaders.indexOf(';');
-  cookieHeaders = cookieHeaders.substr(0, posSemicolon);
-  return cookieHeaders;
 };
 
 exports.getEstateGuru = getEstateGuru;
