2fe3f2d487f2a2bb63e9b248a1660361c089c0cc
Aydin Tekin <aydin.tekin@ibsolution.de>
2023-02-07 01:32:36 +0100

    fixed peerberry grabber

--- public/js/grabber/peerberrygrabber.js
+++ public/js/grabber/peerberrygrabber.js
@@ -2,12 +2,15 @@ const fetch = require('node-fetch');
 const api = 'https://api.peerberry.com/v1/investor/overview';
 const oAuthAPI = 'https://api.peerberry.com/v1/investor/login';
 const headers = {
-  'Content-Type': 'application/json',
+  'content-type': 'application/json;charset=UTF-8',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 };
 
 const getPeerBerry = async (username, password) => {
   const retAuthData = await getAuthToken(username, password);
-  const response = await fetch(api, {method: 'GET', headers: {authorization: 'Bearer ' + retAuthData.access_token}});
+  let headerGet = headers;
+  headerGet.authorization = 'Bearer ' + retAuthData.access_token;
+  const response = await fetch(api, {method: 'GET', headers: headerGet});
   const json = await response.json();
   if (response.status > 200) {
     throw Error('Unexpected response status ' + response.status);
